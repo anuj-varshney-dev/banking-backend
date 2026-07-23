727a8a8cb8874ccc50985fa0aad515ffc6aff99a
Anuj Varshney <helloanujvarshney@gmail.com>
2026-07-23 18:59:45 +0530

    some changes

--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -3,12 +3,15 @@ import { authMiddleware } from "../middleware/authMiddleware.js";
 import { createAccount,
         getAccounts,
         depositMoney,
+        withdrawMoney,
         transferMoney
  } from "../controllers/accountController.js";
 const router=express.Router();
 
 router.post("/create", authMiddleware, createAccount);
 router.post("/deposit", authMiddleware, depositMoney);
+router.post("/withdraw", authMiddleware, withdrawMoney);
+router.post("/transfer", authMiddleware, transferMoney);
 
 export default router;
 
